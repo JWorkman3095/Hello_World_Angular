66c4adace2f252e0f035e16011da25513f8b9de6
Jeff Workman <jworkhollr@aol.com>
2022-03-30 21:09:18 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MaxLengthValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world-angular';
+  title = 'The Bootcamp';
+  maxOn: boolean = true;
+  names: string[] = [ "Ann", "Bob", "Chris", "Dale", "june" ];
+  customers: any[] = [
+    {id: 1, name: "MAX", sales: 10000, state: "OH" },
+    {id: 2, name: "MPG", sales: 30000, state: "OH" },
+    {id: 3, name: "Kroger", sales: 20000, state: "OH" }
+  ];
+
+  aString1: string = "";
+  aString2: string = "";
+
+  copy(): void {
+    this.aString1 = this.aString2
+  }
+
+  clicked(): void{
+    if(this.maxOn) {
+      this.title = "The MAX bootcamp";
+      this.maxOn = !this.maxOn;
+    } else {
+      this.maxOn = !this.maxOn;
+    }
+    
+  }
 }
